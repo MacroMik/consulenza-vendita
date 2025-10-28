d659dbd0ff10ca709626661fec26d534571c63ce
MacroMik <macrorainbow@icloud.com>
2025-10-28 23:20:30 +0100

    Update index.ts

--- supabase/functions/create-checkout-session/index.ts
+++ supabase/functions/create-checkout-session/index.ts
@@ -1,10 +1,11 @@
-import { serve } from 'https://deno.land/std@0.192.0/http/server.ts'
-import Stripe from 'https://esm.sh/stripe@12.12.0?target=deno'
+// @ts-ignore – esm.sh module resolution
+import Stripe from 'https://esm.sh/stripe@12.12.0?target=deno&deno-std=0.177.0'
 
-serve(async (req) => {
-  const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY')!, {
-    apiVersion: '2023-10-16',
-  })
+export default async (req: Request) => {
+  const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY')!,
+    {
+      apiVersion: '2023-10-16',
+    })
 
   try {
     const { price, description } = await req.json()
@@ -36,4 +37,4 @@ serve(async (req) => {
       { status: 400, headers: { 'Content-Type': 'application/json' } },
     )
   }
-})
+}
